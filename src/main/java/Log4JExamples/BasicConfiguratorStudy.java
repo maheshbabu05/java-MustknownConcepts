665326484ae518e65919d6f66115e7c4087d9ad2
Mahesh Babu <Mahesh Babu@MaheshBoard>
2023-10-26 21:21:40 +0530

    Log4J ONE

---
+++ src/main/java/Log4JExamples/BasicConfiguratorStudy.java
@@ -0,0 +1,15 @@
+package Log4JExamples;
+
+import org.apache.logging.log4j.Logger;
+
+public class BasicConfiguratorStudy {
+	
+	
+	//static Logger logger = Logger.class(BasicConfiguratorStudy.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
